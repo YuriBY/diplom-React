07ff59443a0898cefe62e0f8707472776ccd5ed5
ALEX\Admin <vv0325056@gmail.com>
2023-09-17 23:36:53 +0300

    add header

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,36 @@
+import bookstore from "../../assets/Bookstore.png";
+import iconSearch from "../../assets/Icon-Search.png";
+import box from "../../assets/box.png";
+import heart from "../../assets/heart.png";
+import man from "../../assets/man.png";
+
+export const Header = () => {
+  return (
+    <>
+      <div className="flex flex-row justify-between w-3/5 h-28 m-auto ">
+        <div className="w-32 h-7 py-10">
+          <img src={bookstore} alt="" className="" />
+        </div>
+        <div className="w-6/12 h-14 my-6 flex">
+          <div className="relative w-full">
+            <input
+              type="search"
+              placeholder="Search"
+              className="w-full h-full pr-8 pl-[15px] border-solid border-2"
+            />
+            <img
+              src={iconSearch}
+              alt="Search"
+              className="absolute top-3 right-0 h-8 p-2 cursor-pointer"
+            />
+          </div>
+        </div>
+        <div className="w-1/6 h-14 my-6 flex flex-row">
+          <img src={heart} alt="" />
+          <img src={box} alt="" />
+          <img src={man} alt="" className="w-6 h-6 my-[14px] ml-2" />
+        </div>
+      </div>
+    </>
+  );
+};
