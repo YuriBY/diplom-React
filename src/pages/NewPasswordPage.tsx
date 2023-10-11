9ab9b294f3787b9ab7a35f0f6c4a19c79c3778e9
YuriBY <vv0325056@gmail.com>
2023-10-11 15:35:19 +0300

    add reset password

--- src/pages/NewPasswordPage.tsx
+++ src/pages/NewPasswordPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Button } from "../components/Button/Button";
-import { useForm} from "react-hook-form";
+import { useForm, SubmitHandler} from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { Inputs } from "./../components/SignIn/SignInOrSignUp";
 
@@ -10,17 +10,15 @@ interface NewPassInputs {
 }
 
 export const NewPasswordPage = () => {
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-      } = useForm<NewPassInputs>();
-    const navigate = useNavigate();
-    const [checkPassword, setCheckPassword] = useState(false);
-       
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<NewPassInputs>();
 
-    const saveNewPassToLocalStoridge = handleSubmit((data) => {
+    const onSubmit: SubmitHandler<NewPassInputs> = (data) => {
+      
       const userDataString = localStorage.getItem("users");
       console.log(userDataString);      
       const userEmail = localStorage.getItem("emailTOreset");
@@ -43,14 +41,46 @@ export const NewPasswordPage = () => {
           localStorage.setItem("emailTOreset", ''); 
           navigate('/')
             } 
-      }      
-    });      
+      } 
+
+    }
+    
+    const navigate = useNavigate();
+    const [checkPassword, setCheckPassword] = useState(false);
+       
+
+    // const saveNewPassToLocalStoridge = handleSubmit((data) => {
+      
+    //   const userDataString = localStorage.getItem("users");
+    //   console.log(userDataString);      
+    //   const userEmail = localStorage.getItem("emailTOreset");
+    //   console.log(userEmail);
+      
+    //   if (data.password !== data.confirmpassword) {
+    //     setCheckPassword(true);
+    //   } else {
+    //     if (userDataString !== null) {
+    //       const tempArr = JSON.parse(userDataString);
+    //       const objIndex = tempArr.findIndex(
+    //           (obj: Inputs) => obj.email == userEmail
+    //         );
+    //       console.log(objIndex);
+          
+    //       tempArr[objIndex].password = data.password;
+    //       console.log(tempArr);
+          
+    //       localStorage.setItem("users", JSON.stringify(tempArr));
+    //       localStorage.setItem("emailTOreset", ''); 
+    //       navigate('/')
+    //         } 
+    //   }      
+    // });      
      
 
     return (
-        <form onSubmit={saveNewPassToLocalStoridge}
-        className="border-2 border-solid w-[544px] h-[432px] m-auto mt-[140px] mb-[340px] flex flex-col"
-        
+        <form 
+        className="border-2 border-solid w-[544px] h-[432px] m-auto mt-[140px] mb-[340px] flex flex-col" 
+        onSubmit={handleSubmit(onSubmit)}        
       >
         <div className="uppercase font-bebas font-bold mt-10 ml-8 text-2xl">new password</div>
         <label
@@ -59,8 +89,8 @@ export const NewPasswordPage = () => {
         >
           New Password
         </label>
-        <input
-          {...register("password", { required: true, pattern: /^\S+@\S+$/i })}
+        <input type="password"
+          {...register("password", { required: true })}
           className="w-[480px] h-14 border-2 border-solid border-[#E7E7E7] mx-auto text-[#A8A8A8] pl-[20px] font-helios"
           placeholder="Your password"
           onChange={() => setCheckPassword(false)}
@@ -71,8 +101,8 @@ export const NewPasswordPage = () => {
         >
           Confirm Password
         </label>
-        <input
-          {...register("confirmpassword", { required: true, pattern: /^\S+@\S+$/i })}
+        <input type="password"
+          {...register("confirmpassword", { required: true })}
           className="w-[480px] h-14 border-2 border-solid border-[#E7E7E7] mx-auto text-[#A8A8A8] pl-[20px] font-helios"
           placeholder="Confirm your password"
           onChange={() => setCheckPassword(false)}
